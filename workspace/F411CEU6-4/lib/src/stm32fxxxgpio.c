a263b09194ea65be6ccd6488dee51df1fcf355b5
Sergio Santos <sergi@Victus>
2025-09-21 12:58:47 +0100

    stm32f411eu6 refresh_2

--- workspace/F411CEU6-4/lib/src/stm32fxxxgpio.c
+++ workspace/F411CEU6-4/lib/src/stm32fxxxgpio.c
@@ -115,6 +115,7 @@ void GPIOA_af(uint8_t pin, uint8_t af)
 
 /*** HANDLER ***/
 static STM32FXXX_GPIOA stm32fxxx_gpioa = {
+	.instance = GPIOA,
     // V-table
     .clock = GPIOA_clock,
     .moder = GPIOA_moder,
