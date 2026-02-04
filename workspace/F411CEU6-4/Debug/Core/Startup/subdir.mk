2f978ce4d7669427ef8e7fabe5dcaf756a140046
Sergio Santos <sergi@Victus>
2026-02-04 13:33:32 +0000

    fixup

--- workspace/F411CEU6-4/Debug/Core/Startup/subdir.mk
+++ workspace/F411CEU6-4/Debug/Core/Startup/subdir.mk
@@ -15,7 +15,7 @@ S_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
+Core/Startup/startup_stm32f411ceux.o: D:/Embedded/STM32F411CEU6/workspace/F411CEU6-4/Core/Startup/startup_stm32f411ceux.s Core/Startup/subdir.mk
 	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
 clean: clean-Core-2f-Startup
