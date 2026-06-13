bbeceb09e166a81b34bdc034078f2419332830db
Sergio Santos <sergi@Victus>
2026-06-13 14:48:47 +0100

    remove 64x64 font

--- workspace/F411CEU6-4/lib/src/st7789_fonts.h
+++ workspace/F411CEU6-4/lib/src/st7789_fonts.h
@@ -28,16 +28,13 @@ Date:     23/01/2026
 #define ST77XX_YELLOW 0xFFE0
 #define ST77XX_ORANGE 0xFC00
 #define ST77XX_GOLD 0xFFD0
-#define ST77XX_GREY 0xAD55
-#define ST77XX_LIGHTGREY 0xCE99
-#define ST77XX_DARKGREY 0x3186
 
 extern const uint8_t font8x12[] PROGMEM;
 extern const uint8_t font16x16[] PROGMEM;
 extern const uint8_t font24x24[] PROGMEM;
 extern const uint8_t font32x32[] PROGMEM;
 extern const uint8_t font48x48[] PROGMEM;
-extern const uint8_t font64x64[] PROGMEM;
+//extern const uint8_t font64x64[] PROGMEM;
 extern const uint8_t star36x36_outline[180] PROGMEM;
 extern const uint8_t star36x36_filled[180] PROGMEM;
 extern const uint8_t star42x42_filled[252] PROGMEM;
